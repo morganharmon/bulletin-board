3e8a22e569a421b4c58471889fdbedc69a61bd9a
Morgan Harmon <morganharmon@gmail.com>
2022-05-17 12:24:04 -0700

    Copy log out functions from previous project

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 // import functions and grab DOM elements
 
+import { logOut } from './fetch-utils.js';
+
 // let state
 const signInButton = document.getElementById('sign-in-button');
+const logOutButton = document.getElementById('log-out-button')
 const createPostButton = document.getElementById('create-post-button');
 const postSec = document.getElementById('postSec');
 
@@ -11,4 +14,9 @@ signInButton.addEventListener('click', () => {
 });
 createPostButton.addEventListener('click', () => {
     window.location.href = './create post';
+});
+
+logOutButton.addEventListener('click', async () => {
+    await logOut();
+
 });
--- fetch-utils.js
+++ fetch-utils.js
@@ -32,3 +32,8 @@ export async function signInUser(email, password) {
     }
 
 }
+
+export async function logOut() {
+    await client.auth.signOut();
+    return (window.location.href = '/');
+}
